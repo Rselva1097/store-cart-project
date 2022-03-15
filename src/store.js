ddc448e3c50531986d7878d1cc56d7b152903429
Rselva1097 <99159175+Rselva1097@users.noreply.github.com>
2022-03-15 17:17:36 +0530

    first commit

---
+++ src/store.js
@@ -0,0 +1,17 @@
+import { getStorageItem, setStorageItem } from './utils.js';
+let store = getStorageItem('store');
+const setupStore = (products) => {
+    store = products.map((product)=>{
+        const {id,fields:{featured,name,price,company,colors,image:img}} = product
+        const image = img[0].thumbnails.large.url
+        return {id,featured,name,price,company,colors,image}
+    })
+    setStorageItem('store', store)
+};
+
+const findProduct = (id) => {
+    let product = store.find((product)=>product.id === id)
+    console.log(product);
+    return product
+};
+export { store, setupStore, findProduct };
